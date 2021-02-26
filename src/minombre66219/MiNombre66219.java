ef87346d49fb12d67450908a3527f1575b9b3d69
chent <chent@LAPTOP-MPI3AJB6>
2021-02-25 20:10:26 -0600

    puse mi nombre

---
+++ src/minombre66219/MiNombre66219.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minombre66219;
+
+/**
+ *
+ * @author chent
+ */
+public class MiNombre66219 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         System.out.println("Vicente Damian Gonzalez Chable");
+    }
+    
+}
